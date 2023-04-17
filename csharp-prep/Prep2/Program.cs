bd5aaa17de950f7376d6aa7528ceb02273e996c3
jayhklasiq <klasik@byui.edu>
2023-04-17 06:57:39 +0000

    C# Prep 1 and Prep 2 completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage(%): ");
+        string gradePercent = Console.ReadLine();
+        int userGrade = int.Parse(gradePercent);
+
+        string letter = "";
+
+        if (userGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (userGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (userGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (userGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+            
+        if (userGrade >= 70)
+        {
+            Console.WriteLine($"Your grade is {letter}. You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}. Sorry. You didn't pass this course.");
+        }
     }
 }

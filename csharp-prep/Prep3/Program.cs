088866d2c72afdf631a148ff379ed5f57b6f9e18
jayhklasiq <klasik@byui.edu>
2023-04-23 13:08:02 +0000

    Prep 3 and Prep 4 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        gameGuessing();
+        //After the game is over, ask the user if they want to play again. 
+        //Then, loop back and play the whole game again and continue this 
+        //loop as long as they keep saying "yes". at the end of the game.
+        Console.Write("Do you want to play again? ");
+        string response = Console.ReadLine();
+        while (response == "yes")
+        {
+            gameGuessing();
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+        }
+    }    
+    static void gameGuessing()
+    {
+        //1. Start by asking the user for the magic number.
+        // Console.Write("What is the magic number? ");
+        // string mNum = Console.ReadLine();
+        // int magicNum = int.Parse(mNum);
+        //3. Instead of having the user supply the magic number, 
+        //generate a random number from 1 to 100.
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+        //1a. Ask the user for a guess.
+        Console.Write("What is your guess? ");
+        string g = Console.ReadLine();
+        int guess = int.Parse(g);
+        int total = 0;
+        //1b. Using an if statement, determine if the user 
+        //needs to guess higher or lower next time, or 
+        //tell them if they guessed it.
+        //2. Add a loop that keeps looping as long as the
+        // guess does not match the magic number.
+        while (magicNum != guess)
+        {
+        if (guess < magicNum)
+        {
+           Console.WriteLine("Higher");
+           Console.Write("What is your guess? ");
+            g = Console.ReadLine();
+            guess = int.Parse(g);
+        }
+        else if (guess > magicNum)
+        {
+            Console.WriteLine("Lower");
+            Console.Write("What is your guess? ");
+            g = Console.ReadLine();
+            guess = int.Parse(g);
+        }
+        //Keep track of how many guesses the user has made and 
+        //inform them of it at the end of the game.
+        total = total + 1;
+        }
+        if (magicNum == guess)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        Console.WriteLine($"You've made {total} guess(es).");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Ask the user for a series of numbers, and 
+        //Stop when they enter 0.
+        Console.WriteLine("Enter a list of numbers. Enter '0' when you're done.");
+        Console.Write("Enter a number: ");
+        string num = Console.ReadLine();
+        int number = int.Parse(num);
+        //append each one to a list.
+        List<int> numberList = new List<int>();
+        numberList.Add(number);
+        // Compute the sum, or total, of the numbers in the list.
+        int sum = 0;
+        float total = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            num = Console.ReadLine();
+            total = total + 1;
+            number = int.Parse(num);
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+        }
+        
+        foreach (int no in numberList)
+        {
+            sum += no;
+        }
+        int maxValue = numberList.Max();
+        Console.WriteLine(sum);
+        // Compute the average of the numbers in the list.
+        Console.WriteLine($"The average is: {sum / total}");
+        //Find the maximum, or largest, number in the list
+        Console.WriteLine($"The highest value is: {maxValue}");
+        //Sort the numbers in the list and display the new, sorted list (Stretch Challenge 2)
+        numberList.Sort();
+        foreach  (int no in numberList)
+        {
+            Console.WriteLine(no);
+        }
     }
 }
